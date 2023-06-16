f086e6ee166e46f56571cb9fc6bdef532bdeed6d
YashashreeMeshram <yashumeshram124@gmail.com>
2023-06-15 23:19:36 -0700

    example of inheritance

---
+++ JAVA PROGRAM/carinheritance.java
@@ -0,0 +1,16 @@
+class Car{
+int seats;
+int speed ;
+int acclearated;
+}
+class Electricar{
+string battery;
+double percntage;
+int charge duration;
+}
+class Gasolinecar{
+int tank capacity;
+int loading;
+string gasoline left;
+}
+ 
---
+++ JAVA PROGRAM/pictureinheritance.java
@@ -0,0 +1,16 @@
+class Picture {
+string title;
+long price;
+long update price;
+}
+class Photograph extends Picture {
+string photograph name;
+string camera type;
+double height;
+double width;
+}
+class Painting extends Picture {
+string artist name;
+string type;
+double size;
+}
