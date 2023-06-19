8f68f7eb33e836b0ee5a4e048e741b52aed142b9
YashashreeMeshram <yashumeshram124@gmail.com>
2023-06-18 21:44:42 -0700

    argument return type

---
+++ JAVA PROGRAM/DivisionArguReturntype.java
@@ -0,0 +1,10 @@
+class DivisionArguReturntype {
+public int divide(int number1,int number2) {
+return number1/number2;
+}
+public static void main(String[] args) {
+DivisionArguReturntype add= new DivisionArguReturntype();
+int div=add.divide(30,20);
+System.out.println("division of two numbers="+div);
+}
+}
---
+++ JAVA PROGRAM/MultiplicationArguReturntype.java
@@ -0,0 +1,10 @@
+class MultiplicationArguReturntype {
+public int miultiply(int number1,int number2) {
+return number1*number2;
+}
+public static void main(String[] args) {
+MultiplicationArguReturntype add= new MultiplicationArguReturntype();
+int mul=add.multiply(30,20);
+System.out.println("multiplication of two numbers="+mul);
+}
+}
