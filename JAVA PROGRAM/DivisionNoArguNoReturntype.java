e5234ebd63a2df383933b420cf1a4411620463b6
YashashreeMeshram <yashumeshram124@gmail.com>
2023-06-19 01:13:37 -0700

    no argument no return

---
+++ JAVA PROGRAM/DivisionNoArguNoReturntype.java
@@ -0,0 +1,11 @@
+class DivisionNoArguNoReturntype {
+public void divide() {
+int number1=10, number2=5,div;
+div=number1/number2;
+System.out.println("Division of two numbers"+div);
+}
+public static void main(String[] args) {
+DivisionNoArguNoReturntype add= new DivisionNoArguNoReturntype();
+add.divide();
+}
+}
---
+++ JAVA PROGRAM/MultiplicationNoArguNoReturntype.java
@@ -0,0 +1,11 @@
+class MultiplicationNoArguNoReturntype {
+public void multiply() {
+int number1=10, number2=5,mul;
+mul=number1*number2;
+System.out.println("multiplication of two numbers"+mul);
+}
+public static void main(String[] args) {
+MultiplicationNoArguNoReturntype add= new MultiplicationNoArguNoReturntype();
+add.multiply();
+}
+}
