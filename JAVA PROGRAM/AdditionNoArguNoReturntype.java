5e58f5890b449c9e8a9f3d7fa6bb29d57ab1aab2
YashashreeMeshram <yashumeshram124@gmail.com>
2023-06-18 23:48:53 -0700

    addition no argument no return

---
+++ JAVA PROGRAM/AdditionNoArguNoReturntype.java
@@ -0,0 +1,12 @@
+class AdditionNoArguNoReturntype {
+public void addition() {
+int number1=10, number2=5,sum;
+sum=number1+number2;
+System.out.println("addition of two numbers"+sum);
+}
+public static void main(String[] args) {
+AdditionNoArguNoReturntype add= new AdditionNoArguNoReturntype();
+add.addition();
+}
+}
+
