fe4d9bc1b428f01d1edf71789112bfba03b77244
YashashreeMeshram <yashumeshram124@gmail.com>
2023-06-19 00:41:40 -0700

    substraction no argument no return

---
+++ JAVA PROGRAM/SubstractionNoArguNoReturntype.java
@@ -0,0 +1,11 @@
+class SubstractionNoArguNoReturntype {
+public void substraction() {
+int number1=10, number2=5,sub;
+sub=number1-number2;
+System.out.println("substraction of two numbers"+sub);
+}
+public static void main(String[] args) {
+SubstractionNoArguNoReturntype add= new SubstractionNoArguNoReturntype();
+add.substraction();
+}
+}
