17b7d8a7719c06a8629d998fbb053d570905c0f9
YashashreeMeshram <yashumeshram124@gmail.com>
2023-06-28 11:33:52 +0530

    data type program

---
+++ JAVA PROGRAM/Data type/IntDatatype.java
@@ -0,0 +1,10 @@
+class IntDatatype {
+public int method( int number1) {
+return number1;
+}
+  public static void main(String[] args) {
+   IntDatatype data=new IntDatatype();
+    int range=data.method(2146387);	
+    System.out.println("integer number is valid" +range);  
+  }
+}
