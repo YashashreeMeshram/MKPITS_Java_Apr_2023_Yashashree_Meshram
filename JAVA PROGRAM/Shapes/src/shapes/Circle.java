79311645d3bd481eb680596a8fd6fe2d56352d4f
YashashreeMeshram <yashumeshram124@gmail.com>
2023-08-05 14:20:18 +0530

    example of shapes MyProject2

---
+++ JAVA PROGRAM/Shapes/src/shapes/Circle.java
@@ -0,0 +1,16 @@
+package shapes;
+
+    public class Circle extends Shape {
+        private int radius;
+        public Circle(int radius){
+            this. radius=radius;
+        }
+        @Override
+
+        public void calArea(){
+            System.out.println ("area of circle :" +(3.14 * radius*radius));
+
+
+        }
+    }
+
