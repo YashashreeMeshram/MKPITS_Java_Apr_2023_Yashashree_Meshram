f48b7d2e620bec050b17737034e941454c4d19b1
YashashreeMeshram <yashumeshram124@gmail.com>
2023-08-07 12:01:46 +0530

    interface example 2 Calculation

---
+++ JAVA PROGRAM/Interface/calci interface/src/calci/Calculation.java
@@ -0,0 +1,7 @@
+package calci;
+
+public interface Calculation {
+    public abstract int calculation(int a,int b);
+}
+
+
---
+++ JAVA PROGRAM/Interface/calci interface/src/calci/Substraction.java
@@ -0,0 +1,9 @@
+package calci;
+
+public class Substraction implements Calculation {
+    @Override
+    public int calculation(int a, int b)
+    {
+        return a-b;
+    }
+}
