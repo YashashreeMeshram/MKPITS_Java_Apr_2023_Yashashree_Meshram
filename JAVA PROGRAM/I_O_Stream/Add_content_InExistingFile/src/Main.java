e8d0ea3c2e1c4ff7337b9f649057a3594a8e953a
YashashreeMeshram <yashumeshram124@gmail.com>
2023-08-22 15:06:49 +0530

    add content to an existing file by using append

---
+++ JAVA PROGRAM/I_O_Stream/Add_content_InExistingFile/src/Main.java
@@ -0,0 +1,10 @@
+import stud.*;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+         ReadWriteObject.writeToFile("xyz.txt");
+    }
+}
